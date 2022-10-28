792cd9dbe974e1af4e72f0c853bc1b7a39c89d4f
AuroraHan <1224195854@qq.com>
2022-10-29 00:07:10 +0800

    测试规范

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,31 @@
     </template>
     默认内容
   </MCard>
+  <div :class="[ns.b()]">
+    <div :class="[ns.b('childer')]"></div>
+    <div :class="[ns.b('childer')]"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import MCard from "@/components/card/MCard.vue";
+import { useNamespace } from "@/hooks/use-namespace";
+
+const ns = useNamespace("main");
 </script>
 
 <style scoped lang="scss">
+@include block(main) {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+  @include block(main-childer) {
+    width: 100px;
+    height: 100px;
+    background-color: $border-color-darker;
+  }
+}
+
 .m-cont {
   width: 400px;
   height: 300px;
