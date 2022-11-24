bf2ca282793e5b30febda738ee0d56b0e7e53c49
guoxiaohan <guoxiaohan@pybctec.com>
2022-11-24 20:45:47 +0800

    添加滑动

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <MCard shadow="hover" :class="[ns.b()]">
+  <!-- <MCard shadow="hover" :class="[ns.b()]">
     <template #header>
       <div>
         头部内容头部内头部内容内容头部内容头部内容内容头部内容头部内容内容头部内容头部内容
       </div>
     </template>
     默认内容
-  </MCard>
+  </MCard> -->
   <!-- <MAvatar size="100px" shape="square" :src="state.url"></MAvatar>
   <br />
   <MAvatar shape="circle">User</MAvatar>
   <br /> -->
-  <MAvatar size="100px" shape="square" :src="state.url" @error="errorHandler">
+  <!-- <MAvatar size="100px" shape="square" :src="state.url" @error="errorHandler">
     <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
   </MAvatar>
-  <MyTest></MyTest>
+  <MyTest></MyTest> -->
+  <MSlider></MSlider>
 </template>
 
 <script setup lang="ts">
@@ -22,6 +23,7 @@ import { reactive } from "vue";
 import MCard from "@/components/card/MCard.vue";
 import MyTest from "@/components/mytest/index.vue";
 import MAvatar from "@/components/avatar/MAvatar.vue";
+import MSlider from "@/components/slider/MSlider.vue"
 import { useNamespace } from "@/hooks/use-namespace";
 
 const ns = useNamespace("main");
