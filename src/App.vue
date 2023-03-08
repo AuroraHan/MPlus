c46d2b1d59bc785c54da951c1ae0cdff0ad9fef7
guoxiaohan <guoxiaohan@pybctec.com>
2023-03-08 18:16:00 +0800

    样式优化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <h1>主页面</h1>
-  <router-view></router-view>
+  <div :class="ns.b()">
+    <div :class="ns.e('left')">left</div>
+    <div :class="ns.e('middle')">
+      <router-view></router-view>
+    </div>
+    <div :class="ns.e('right')">right</div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -21,5 +26,28 @@ onMounted(() => {
 </script>
 
 <style scoped lang="scss">
+$ns: main;
 
+@include block($ns) {
+  display: flex;
+  width: 100%;
+  height: 400px;
+
+  @include element(left) {
+    width: 200px;
+    background-color: chartreuse;
+    flex-shrink: 0;
+  }
+
+  @include element(middle) {
+    width: 100%;
+    flex: 1;
+  }
+
+  @include element(right) {
+    width: 200px;
+    background-color: rgb(34, 216, 216);
+    flex-shrink: 0;
+  }
+}
 </style>
